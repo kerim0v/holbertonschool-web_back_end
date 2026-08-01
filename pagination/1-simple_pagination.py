9974c76404111899910063d349d0069d1ca89fbf
kerim0v <kerimvtamerlan@gmail.com>
2026-08-01 18:39:32 +0400

    a

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Module for calculating pagination index ranges."""
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """asdadd"""
+    start_index = (page - 1) * page_size
+    end_i = start_index + page_size
+    return (start_index, end_i)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """asda"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_i, end_i = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_i >= len(data):
+            return []
+
+        return data[start_i:end_i]
